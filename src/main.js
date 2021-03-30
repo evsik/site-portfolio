bb5b846e974c4e10fc8906c05bdca9337f43a647
evsik <evsevsikov@gmail.com>
2021-03-30 20:50:01 +0300

    added bootstrap node_modules

--- src/main.js
+++ src/main.js
@@ -2,8 +2,19 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
 Vue.config.productionTip = false
+
+import bootstrap from "bootstrap"
+import "bootstrap/dist/css/bootstrap.min.css"
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+import "./layout/styles/style.css";
 
 new Vue({
   router,
