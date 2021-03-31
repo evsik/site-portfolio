6e73ea4f0baeeff33e8e6cd42064371ec90820d5
evsik <evsevsikov@gmail.com>
2021-03-31 17:37:38 +0300

    added Header.vue comp

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <Header></Header>
+<!--    <div id="nav">-->
+<!--      <router-link to="/">Home</router-link>-->
+<!--      |-->
+<!--      <router-link to="/main">Main</router-link>-->
+<!--    </div>-->
     <router-view/>
   </div>
 </template>
+
+<script>
+import Header from "./components/Header/Header";
+
+export default {
+  name: "App",
+  components: {
+    Header
+  }
+}
+</script>
 
 <style lang="scss">
 @import '../node_modules/bootstrap/scss/bootstrap.scss';
---
+++ src/components/Header/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div class="container">
+
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "Header"
+}
+</script>
+
+<style scoped lang="scss">
+@import '../../../node_modules/bootstrap/scss/bootstrap.scss';
+@import '../../../node_modules/bootstrap-vue/src/index.scss';
+
+
+</style>
