92c04e4ba6ae5ed0c950199ab2e2642e26bb25e3
evsik <evsevsikov@gmail.com>
2021-04-01 12:13:06 +0300

    added Info.vue comp

--- src/components/Slider/Info.vue
+++ src/components/Slider/Info.vue
@@ -1,5 +1,9 @@
 <template>
-
+  <p class="b-briefInfo">
+    Front-end Developer
+    <br>
+    27 years old, Moscow
+  </p>
 </template>
 
 <script>
@@ -9,5 +13,10 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.b-briefInfo {
+  font-family: Gilroy, serif;
+  font-size: 18px;
+  line-height: 22px;
+  color: #070707;
+}
 </style>
