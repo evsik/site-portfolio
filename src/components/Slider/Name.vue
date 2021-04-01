3d2707b451582b4ae3e51ec00ba322878dc3874e
evsik <evsevsikov@gmail.com>
2021-04-01 12:06:03 +0300

    added Name.vue comp

---
+++ src/components/Slider/Name.vue
@@ -0,0 +1,22 @@
+<template>
+  <h1 class="b-name">
+    Ilia
+    <br>
+    Evsikov
+  </h1>
+</template>
+
+<script>
+export default {
+  name: "Name"
+}
+</script>
+
+<style scoped lang="scss">
+.b-name {
+  font-family: Gilroy, serif;
+  font-size: 47.3392px;
+  line-height: 59px;
+  color: #070707;
+}
+</style>
---
+++ src/components/Slider/Slider.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="b-slider">
+    <div class="container">
+      <Name></Name>
+    </div>
+  </div>
+</template>
+
+<script>
+import Name from "./Name";
+
+export default {
+  name: "Slider",
+  components: {
+    Name
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.b-slider {
+  display: flex;
+  justify-content: space-between;
+  padding: 30px 0 30px 0;
+}
+</style>
