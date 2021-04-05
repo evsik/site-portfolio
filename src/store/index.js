358c98e57286a16550fa69d0618e170532b8eaff
evsik <evsevsikov@gmail.com>
2021-04-05 16:59:18 +0300

    skills.json

--- src/store/index.js
+++ src/store/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import {get} from '../utils/reqs'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
+    state: {
+        dataArr: [],
+        dataUrl: 'https://raw.githubusercontent.com/evsik/Landing-with-video/master/photosData.json',
+    },
+    mutations: {
+        getData(state) {
+            get(state.dataUrl)
+                .then(data => {
+                    state.dataArr = data
+                });
+        }
+    },
+    actions: {},
+    modules: {}
 })
