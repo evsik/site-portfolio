67cdca5548051faf19d73f9c44eb5c320233a7af
evsik <evsevsikov@gmail.com>
2021-04-12 13:19:18 +0300

    adaptive for header ( xs )

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -21,5 +21,11 @@ export default {
 @import '../../../node_modules/bootstrap/scss/bootstrap.scss';
 @import '../../../node_modules/bootstrap-vue/src/index.scss';
 
+header {
+  @include media-breakpoint-down(xs) {
+    display: none !important;
+  }
+}
+
 
 </style>
